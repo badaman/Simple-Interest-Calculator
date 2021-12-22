9b205e4da2258b937e9796d86184f8bd939b2251
Aman <amanjaipuria086@gmail.com>
2021-12-22 21:42:16 +0530

    highlight

--- vftvk-Simple-Interest-Calculator-master/script.js
+++ vftvk-Simple-Interest-Calculator-master/script.js
@@ -1,7 +1,7 @@
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval + "%";
 }
 
 function compute()
@@ -15,6 +15,6 @@ function compute()
         var years = document.getElementById("years").value;
         var interest = principal * years * rate /100;
         var year = new Date().getFullYear() + parseInt(years);
-        document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%.\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + ".\<br\>";
+        document.getElementById("result").innerHTML = `If you deposit \<mark\>${principal}\<\/mark\>,\<br\>at an interest rate of \<mark\>${rate}%\<\/mark\>.\<br\>You will receive an amount of \<mark\>${interest}\<\/mark\>,\<br\>in the year \<mark\>${year}\<\/mark\>.\<br\>`;
     }
 }       
